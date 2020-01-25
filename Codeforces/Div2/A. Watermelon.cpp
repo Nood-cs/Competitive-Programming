b8f8b36c80f868327b529491d42c7b817a60217a
Nada Medhat Mohamed Ibrahim <nada.medhat@outlook.com>
2020-01-26 00:00:55 +0200

    Updated problem solution

--- Codeforces/Div2/A. Watermelon.cpp
+++ Codeforces/Div2/A. Watermelon.cpp
@@ -1,19 +1,24 @@
-//ACCEPTED
+/* ACCEPTED!
+* Time Complexity: O(1)
+* Space Comlexity: O(1)
+* Link: http://codeforces.com/contest/4/problem/A
+*/
+
 #include<iostream>
+#include<string>
 using namespace std;
-int main()
-{
-	int w, res, sub;
-	cin >> w;
-	res = w / 2;
-	sub = w - 2;
-	if ( (( w % 2 == 0) && w > 3) && ( (res % 2==0 ) || (sub %2 ==0 ) )  )
+
+string isEven(int n) {
+	if (n != 2 && n % 2 == 0)
 	{
-		cout << "YES" << endl;
+		return "YES";
 	}
-	else
-	{
-		cout << "NO" << endl;
-	}
+	return "NO";
+}
+
+int main() {
+	int n;
+	cin >> n;
+	cout << isEven(n) << endl;
 	return 0;
 }
